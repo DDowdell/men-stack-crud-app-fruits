8a91d70cb0a647446f69ce144b32d8148edfd76a
Dae Dowdell <dow_dell2010@yahoo.com>
2025-07-21 15:15:13 -0400

    made update operation

--- server.js
+++ server.js
@@ -79,6 +79,21 @@ app.get("/fruits/:fruitId/edit", async (req, res) => {
   });
 });
 
+//(PUT) EDIT /fruits
+app.put("/fruits/:fruitId", async (req, res) => {
+  // Handle the 'isReadyToEat' checkbox data
+  if (req.body.isReadyToEat === "on") {
+    req.body.isReadyToEat = true;
+  } else {
+    req.body.isReadyToEat = false;
+  }
+  
+  // Update the fruit in the database
+  await Fruit.findByIdAndUpdate(req.params.fruitId, req.body);
+
+  // Redirect to the fruit's show page to see the updates
+  res.redirect(`/fruits/${req.params.fruitId}`);
+});
 
 
 
